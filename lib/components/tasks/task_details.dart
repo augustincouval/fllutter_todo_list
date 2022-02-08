32047014753b9af1714e2cbedb6cab191146f80b
augustin <augustin.couval@viacesi.fr>
2022-02-08 15:40:07 +0100

    structure prijet

--- lib/components/tasks/task_details.dart
+++ lib/components/tasks/task_details.dart
@@ -10,6 +10,10 @@ class TaskDetails extends StatefulWidget {
 class _TaskDetailsState extends State<TaskDetails> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Détail de la tâche'),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:couval_todo_list/components/tasks/task_master.dart';
 import 'package:couval_todo_list/screens/all_tasks.dart';
 import 'package:couval_todo_list/screens/home.dart';
 import 'package:flutter/material.dart';
@@ -12,6 +13,12 @@ class TodoList extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      initialRoute: '/',
+      routes: {
+        '/home': (context) => TodoList(),
+        '/all' : (context) => AllTasks(),
+        '/master' : (context) => TaskMaster(),
+       },
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
